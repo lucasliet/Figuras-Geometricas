76200c907d3355577318453cdc3bc76a9d130da2
Lucas Souza <lucasouliveira@gmail.com>
2020-03-28 13:01:15 -0300

    Implemented Circulo

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,6 +1,12 @@
 public class Circulo extends Figura {
+    private double raio;
+
+    public Circulo(double raio){
+        this.raio = raio;
+    }
+
     @Override
     public double Area() {
-        return 0;
+        return  Math.PI * Math.pow(this.raio, 2);
     }
 }
