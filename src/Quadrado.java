e8b9426a3273ebb13ac0a6f601deffbf2a93ba08
Lucas Souza <lucasouliveira@gmail.com>
2020-03-28 12:39:49 -0300

    implemented Quadrado

--- src/Quadrado.java
+++ src/Quadrado.java
@@ -1,2 +1,16 @@
-public class Quadrado extends Poligono {
+public class Quadrado extends Poligono implements Diagonal{
+
+    public Quadrado(double base, double altura) {
+        super(base, altura);
+    }
+
+    @Override
+    public double Area(){
+        return super.getBase() * super.getAltura();
+    }
+
+    @Override
+    public double calcDiagonal() {
+        return Math.sqrt(Area()) * Math.sqrt(2);
+    }
 }
