88a17acff6a3257609a16c77854a519c5648638f
Lucas Souza <lucasouliveira@gmail.com>
2020-03-28 12:57:15 -0300

    Implemented Triangulo

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,2 +1,10 @@
-public class Triangulo extends Poligono {
+public class Triangulo extends Poligono  {
+    public Triangulo(double base, double altura) {
+        super(base, altura);
+    }
+
+    @Override
+    public double Area() {
+        return ( super.getBase() * super.getAltura() ) / 2;
+    }
 }
